b94d14aa5d6b3e6ba6b6f17be992e16801efabd3
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-09-16 11:08:49 -0300

    Profile page created

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -10,7 +10,7 @@ export class StorageService {
         if(usr == null) {
             return null;
         } else {
-            JSON.parse(usr);
+            return JSON.parse(usr);
         }
     }
     setLocalUser(obj: LocalUser) {
