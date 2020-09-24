9f625d3512293735d2745602a8bf70af946512d3
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-09-24 13:42:45 -0300

    Loading data from product into productDetail page

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { API_CONFIG } from './../../config/api.config';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -7,12 +8,20 @@ import { Observable } from 'rxjs/Rx';
 export class ProdutoService {
     constructor (public http: HttpClient) {      
     } 
-    
+    findById(produto_id: string) {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
     findByCategoria(categoria_id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
     }
     getSmallImageFromBucket(id: string): Observable<any>{
         let url = `${API_CONFIG.bucketUrl}/prod${id}-small.jpg`
         return this.http.get(url, { responseType: 'blob'});
-      }
+    }
+
+    getImageFromBucket(id: string): Observable<any>{
+        let url = `${API_CONFIG.bucketUrl}/prod${id}.jpg`
+        return this.http.get(url, { responseType: 'blob'});
+    }
 }
